6a6b49ca0b567d5e0341f5813a7cf19961b153e7
nayan <nayan.technideus@gmail.com>
2019-07-11 16:21:54 +0530

    copy conflict file

--- src (copy)/app/directives/action-executor.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ActionExecutorDirective } from './action-executor.directive';
-import { ActionExecutorService } from '../services/data-provider.service';
-
-describe('DirectivesDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new ActionExecutorDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
